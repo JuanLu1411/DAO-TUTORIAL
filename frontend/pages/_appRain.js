436276678b763be02c8377af90178e99bc04cb3e
JuanLu1411 <juanlumunoz7@gmail.com>
2023-02-07 11:53:50 +0100

    Primer Commit

--- frontend/pages/_appRain.js
+++ frontend/pages/_appRain.js
@@ -1,26 +1,4 @@
 import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-
-/*
-You can add alchemy provider and configure supported chains.
-
-- To add Alchemy and Add networks :
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
-
-! Make sure not to push the Alchemy Key into Github or any source control.
-*/
-
-// Imports for Rainbowkit and Wagmi
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
 
 // Setting Chains
 const { chains, provider } = configureChains(
